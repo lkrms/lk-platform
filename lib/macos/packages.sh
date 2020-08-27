9966bb67d3f72c62a6d32e4a52347a5787911128
Luke Arms <luke@linacreative.com>
2020-08-27 23:49:20 +1000

    Fix inclusion of mas as cask rather than formula

--- lib/macos/packages.sh
+++ lib/macos/packages.sh
@@ -30,6 +30,7 @@ HOMEBREW_FORMULAE=(
     htop
     jq
     lftp
+    mas
     media-info
     ncdu
     nmap
@@ -43,7 +44,6 @@ HOMEBREW_FORMULAE=(
 
 HOMEBREW_CASKS=(
     iterm2
-    mas
 
     #
     ${HOMEBREW_CASKS[@]+"${HOMEBREW_CASKS[@]}"}
