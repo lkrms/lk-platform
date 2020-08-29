6447a4aaebf4b58b8e8455b659177f1a456b8e2c
Luke Arms <luke@linacreative.com>
2020-08-30 00:42:31 +1000

    Add lk_secret and friends for password storage

--- lib/bash/core.sh
+++ lib/bash/core.sh
@@ -1218,13 +1218,71 @@ if lk_is_macos; then
         # each write)
         script -q -t 0 /dev/null "$@"
     }
+
+    # lk_secret_set VALUE LABEL [SERVICE]
+    function lk_secret_set() {
+        security add-generic-password -a "$USER" -l "$2" -s "${3:-${0##*/}}" -G "$1" -U -w
+    }
+
+    # lk_secret_get VALUE [SERVICE]
+    function lk_secret_get() {
+        security find-generic-password -a "$USER" -s "${2:-${0##*/}}" -G "$1" -w
+    }
+
+    # lk_secret_forget VALUE [SERVICE]
+    function lk_secret_forget() {
+        security delete-generic-password -a "$USER" -s "${2:-${0##*/}}" -G "$1"
+    }
 else
     function lk_tty() {
         local COMMAND=$1
         shift
         script -qfc "$COMMAND$([ $# -eq 0 ] || printf ' %q' "$@")" /dev/null
     }
+
+    # lk_secret_set VALUE LABEL [SERVICE]
+    function lk_secret_set() {
+        secret-tool store --label="$2" -- service "${3:-${0##*/}}" value "$1"
+    }
+
+    # lk_secret_get VALUE [SERVICE]
+    function lk_secret_get() {
+        secret-tool lookup -- service "${2:-${0##*/}}" value "$1"
+    }
+
+    # lk_secret_forget VALUE [SERVICE]
+    function lk_secret_forget() {
+        secret-tool clear -- service "${2:-${0##*/}}" value "$1"
+    }
 fi
+
+# lk_secret VALUE LABEL [SERVICE]
+function lk_secret() {
+    local SERVICE=${3:-${0##*/}} KEYCHAIN=keychain PASSWORD
+    [ -n "${1:-}" ] || lk_warn "no value" || return
+    [ -n "${2:-}" ] || lk_warn "no label" || return
+    lk_is_macos || KEYCHAIN=keyring
+    if ! PASSWORD="$(lk_secret_get "$1" "$SERVICE" 2>/dev/null)"; then
+        if lk_no_input; then
+            lk_console_warning "No password for $SERVICE->$1 found in $KEYCHAIN"
+            return 1
+        fi
+        lk_console_message \
+            "Enter the password for $2 to add it to your $KEYCHAIN"
+        lk_secret_set "$1" "$2" "$SERVICE" &&
+            PASSWORD="$(lk_secret_get "$1" "$SERVICE")" || return
+    fi
+    echo "$PASSWORD"
+}
+
+# lk_remove_secret VALUE [SERVICE]
+function lk_remove_secret() {
+    [ -n "${1:-}" ] || lk_warn "no value" || return
+    lk_secret_get "$@" >/dev/null 2>&1 ||
+        lk_warn "password not found" || return 0
+    lk_secret_forget "$@" || return
+    lk_console_message "Password removed successfully"
+}
 
 function lk_ssl_client() {
     local HOST="${1:-}" PORT="${2:-}" SERVER_NAME="${3:-${1:-}}"
