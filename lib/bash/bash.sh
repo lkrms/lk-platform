9ceceb91824d80860a04a011451591f2ae3239ca
Luke Arms <luke@linacreative.com>
2020-09-01 22:06:50 +1000

    Add lk_bash_udf_defaults

--- lib/bash/bash.sh
+++ lib/bash/bash.sh
@@ -6,3 +6,16 @@ function lk_bash_array_literals() {
         shfmt -tojson |
         jq -r '..|select(type=="object")|.Array|select(. != null).Elems[].Value.Parts[]|select(.Type=="Lit").Value'
 }
+
+# lk_bash_udf_defaults [<STACKSCRIPT_PATH>]
+#
+# Output Bash variable assignments for UDF tags found in the Linode StackScript
+# at STACKSCRIPT_PATH or on standard input.
+#
+function lk_bash_udf_defaults() {
+    cat ${1+"$1"} |
+        grep -E '^.*<UDF name="([^"]+)"' "$1" |
+        sed -E \
+            -e 's/^.*<UDF name="([^"]+)".* default="([^"]*)".*/\1=${\1:-\2}/' \
+            -e 's/^.*<UDF name="([^"]+)".*/\1=${\1:-}/'
+}
