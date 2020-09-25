47225cc41dde3a17caf780da922dfb98a64c209b
Luke Arms <luke@linacreative.com>
2020-09-25 16:55:09 +1000

    Add lk_git_ancestors

--- lib/bash/git.sh
+++ lib/bash/git.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# shellcheck disable=SC2015
+
+# lk_git_ancestors <REF>...
+#
+# Output lines with tab-separated fields <behind>, <hash>, and REF, sorted
+# numerically on <behind>, for each REF that is an ancestor of HEAD. If no REF
+# is an ancestor, return false.
+function lk_git_ancestors() {
+    local REF HASH BEHIND ANCESTORS
+    for REF in "$@"; do
+        git merge-base --is-ancestor "$REF" HEAD &&
+            HASH=$(git merge-base "$REF" HEAD) &&
+            BEHIND=$(git rev-list --count "$HASH..HEAD") || continue
+        ANCESTORS+=("$BEHIND" "$HASH" "$REF")
+    done
+    [ ${#ANCESTORS[@]} -gt 0 ] || return
+    printf '%s\t%s\t%s\n' "${ANCESTORS[@]}" | sort -n
+}
+
 function lk_git_recheckout() {
     local REPO_ROOT COMMIT
     lk_console_message "Preparing to delete the index and check out HEAD again"
