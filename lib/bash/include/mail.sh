07f862bb73f3a37068b6793b7ceef3fcc711673a
Luke Arms <luke@linacreative.com>
2020-10-20 16:44:53 +1100

    Add lk_mail_send

--- lib/bash/include/mail.sh
+++ lib/bash/include/mail.sh
@@ -30,7 +30,7 @@ function lk_mail_attach() {
     _lk_mail_ready || return
     [ -f "$1" ] || lk_warn "file not found: $1" || return
     FILE_NAME=${2:-${1##*/}}
-    MIME_TYPE=${3:-$(file --brief --mime-type "$1")} ||
+    MIME_TYPE=${3:-$(file --brief --mime-type "$(realpath "$1")")} ||
         MIME_TYPE=application/octet-stream
     _LK_MAIL_ATTACH_PATH+=("$1")
     _LK_MAIL_ATTACH_NAME+=("$FILE_NAME")
@@ -55,21 +55,43 @@ function _lk_mail_end_parts() {
     eval "$1="
 }
 
+# lk_mail_get_mime SUBJECT TO [FROM [HEADERS...]]
+#
 # shellcheck disable=SC2097,SC2098
 function lk_mail_get_mime() {
-    local BOUNDARY ALT_BOUNDARY='' i \
+    local SUBJECT TO FROM BOUNDARY ALT_BOUNDARY='' i \
         PREAMBLE="This is a multi-part message in MIME format."
     _lk_mail_ready || return
+    [ $# -ge 2 ] || lk_warn "invalid arguments" || return
+    SUBJECT=$1
+    TO=$2
+    FROM=${3:-${LK_MAIL_FROM-$(
+        ADDRESS=${USER:-nobody}@$(hostname -f) ||
+            ADDRESS=${USER:-nobody}@localhost
+        NAME=$(lk_full_name 2>/dev/null) ||
+            NAME=${USER:-nobody}
+        printf '"%s" <%s>' \
+            "${NAME//\"/\\\"}" \
+            "$ADDRESS"
+    )}}
+    [[ ! "$SUBJECT$TO$FROM" =~ .*[$'\r\n'].* ]] ||
+        lk_warn "line breaks not permitted in SUBJECT, TO, or FROM" ||
+        return
     BOUNDARY=$(lk_random_hex 12)
     cat <<EOF
+From: $FROM
+To: $TO
+Date: $(date -R)
+Subject: $SUBJECT$([ $# -lt 4 ] || printf '\n%s' "${@:4}")
 MIME-Version: 1.0
-Content-Type: multipart/mixed; boundary="$BOUNDARY"
+Content-Type: multipart/mixed; boundary=$BOUNDARY
 
 EOF
+    # TODO: use 7bit if content allows
     [ "${_LK_MAIL_TEXT:+1}${_LK_MAIL_HTML:+1}" != 11 ] || {
         ALT_BOUNDARY=$(lk_random_hex 12)
         ALT_BOUNDARY='' _lk_mail_get_part \
-            "" "multipart/alternative; boundary=\"$ALT_BOUNDARY\""
+            "" "multipart/alternative; boundary=$ALT_BOUNDARY" "8bit"
     }
     [ -z "$_LK_MAIL_TEXT" ] ||
         _lk_mail_get_part "${_LK_MAIL_TEXT%$'\n'}"$'\n' \
@@ -80,6 +102,7 @@ EOF
     [ -z "$ALT_BOUNDARY" ] ||
         _lk_mail_end_parts ALT_BOUNDARY
     for i in "${!_LK_MAIL_ATTACH_PATH[@]}"; do
+        # TODO: implement lk_maybe_encode_header_value
         _lk_mail_get_part "" \
             "$(printf '%s; name="%s"' \
                 "${_LK_MAIL_ATTACH_TYPE[$i]}" \
@@ -90,4 +113,15 @@ EOF
         base64 <"${_LK_MAIL_ATTACH_PATH[$i]}" || return
     done
     _lk_mail_end_parts BOUNDARY
+}
+
+# lk_mail_send SUBJECT TO [FROM [HEADERS...]]
+function lk_mail_send() {
+    local MTA
+    _lk_mail_ready || return
+    [ $# -ge 2 ] || lk_usage "\
+Usage: $(lk_myself -f) SUBJECT TO [FROM [HEADERS...]]" || return
+    MTA=$(lk_command_first_existing sendmail msmtp) ||
+        lk_warn "MTA not found" || return
+    lk_mail_get_mime "$@" | "$MTA" -oi -t
 }
