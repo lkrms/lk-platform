c670179abd2d27debb624c2b2afec50ac088ede8
Luke Arms <luke@linacreative.com>
2021-06-18 15:27:16 +1000

    Fix regression in lk_require_output

--- lib/bash/include/core.sh
+++ lib/bash/include/core.sh
@@ -438,18 +438,15 @@ function lk_fd_next() {
 # Return true if COMMAND writes output other than newlines and exits without
 # error. If -q is set, suppress output.
 function lk_require_output() {
-    local QUIET FD STATUS=0
+    local QUIET FILE
     [ "${1-}" != -q ] || { QUIET=1 && shift; }
-    [ -n "${QUIET-}" ] ||
-        { FD=$(lk_fd_next) && eval "exec $FD>&1"; } || return
-    if [ -n "${FD-}" ]; then
-        "$@" | tee "/dev/fd/$FD"
-    else
-        "$@"
-    fi | grep -E '^.+$' >/dev/null || STATUS=$?
-    [ -z "${FD-}" ] ||
-        eval "exec $FD>&-" || return
-    return "$STATUS"
+    FILE=$(lk_mktemp_file) && lk_delete_on_exit "$FILE" &&
+        if [ -z "${QUIET-}" ]; then
+            "$@" | tee "$FILE"
+        else
+            "$@" >"$FILE"
+        fi &&
+        grep -Eq '^.+$' "$FILE" || return
 }
 
 #### END core.sh.d
