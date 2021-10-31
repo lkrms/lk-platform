d4c61439b0fc96c34e749ae05d0694431a5477ab
Luke Arms <luke@linacreative.com>
2021-10-31 12:09:29 +1100

    Add build-all.sh, fix issue where build-bash.sh fails when minifying

---
+++ src/build-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+_dir=${BASH_SOURCE%${BASH_SOURCE##*/}}
+_dir=${_dir:-$PWD}
+_dir=$(cd "$_dir" && pwd -P)
+
+shfmt_minify=${shfmt_minify:-0} \
+    shfmt_simplify=${shfmt_simplify:-1} \
+    "$_dir/build-bash.sh"
+
+echo >&2
+
+"$_dir/build-json.sh"
