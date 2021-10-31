2658781b2beb826b2f68917d29c620a548ee7fbf
Luke Arms <luke@linacreative.com>
2021-10-31 15:04:42 +1100

    Fix lk_mktemp* regressions

--- lib/bash/include/core.sh
+++ lib/bash/include/core.sh
@@ -860,7 +860,7 @@ function lk_warn() {
 
 function lk_mktemp() {
     local TMPDIR=${TMPDIR:-/tmp} FUNC=${FUNCNAME[1 + ${_LK_STACK_DEPTH:-0}]-}
-    mktemp "$@" ${_LK_MKTEMP_ARGS+"${_LK_MKTEMP_ARGS[@]}"} \
+    mktemp "$@" ${_LK_MKTEMP_ARGS-} \
         "${TMPDIR%/}/${0##*/}${FUNC:+-$FUNC}${_LK_MKTEMP_EXT-}.XXXXXXXXXX"
 }
 
@@ -871,15 +871,15 @@ function lk_mktemp() {
 # its output to the file. If VAR is already set to the path of an existing
 # file and -r ("reuse") is set, proceed without creating a new file.
 function lk_mktemp_with() {
-    local _REUSE= _LK_STACK_DEPTH=$((1 + ${_LK_STACK_DEPTH:-0}))
+    local _REUSE= _DEPTH=$((1 + ${_LK_STACK_DEPTH:-0}))
     [ "${1-}" != -r ] || { _REUSE=1 && shift; }
     [ $# -ge 1 ] || lk_err "invalid arguments" || return
     local _VAR=$1
     shift
     [ -n "${_REUSE-}" ] && [ -e "${!_VAR-}" ] ||
-        { eval "$_VAR=\$(lk_mktemp)" &&
+        { eval "$_VAR=\$(_LK_STACK_DEPTH=\$_DEPTH lk_mktemp)" &&
             lk_delete_on_exit "${!_VAR}"; } || return
-    { [ $# -eq 0 ] || "$@" >"${!_VAR}"; }
+    [ $# -eq 0 ] || "$@" >"${!_VAR}"
 }
 
 # lk_mktemp_dir_with [-r] VAR [COMMAND [ARG...]]
@@ -889,13 +889,14 @@ function lk_mktemp_with() {
 # directory. If VAR is already set to the path of an existing directory and -r
 # ("reuse") is set, proceed without creating a new directory.
 function lk_mktemp_dir_with() {
-    local IFS _ARG=1 _LK_STACK_DEPTH=$((1 + ${_LK_STACK_DEPTH:-0})) \
-        _LK_MKTEMP_ARGS=(-d)
-    unset IFS
-    [ "${1-}" != -r ] || { _ARG=2; }
-    lk_mktemp_with "${@:1:_ARG}" || return
-    local _VAR=${!_ARG}
-    { [ $# -eq "$_ARG" ] || (cd "${!_VAR}" && "${@:_ARG+1}"); }
+    local _ARGS=() _DEPTH=$((1 + ${_LK_STACK_DEPTH:-0}))
+    [ -z "${1+1}" ] || _ARGS[0]=$1
+    [ "${1-}" != -r ] || [ -z "${2+1}" ] || _ARGS[1]=$2
+    _LK_STACK_DEPTH=$_DEPTH _LK_MKTEMP_ARGS=-d \
+        lk_mktemp_with ${_ARGS+"${_ARGS[@]}"} || return
+    local _VAR=${_ARGS[1]-${_ARGS[0]}}
+    shift "${#_ARGS[@]}"
+    [ $# -eq 0 ] || (cd "${!_VAR}" && "$@")
 }
 
 # lk_trap_add SIGNAL COMMAND [ARG...]
