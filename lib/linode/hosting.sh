35b47ca9182f57e9519ad41a3ff442286c4fcf59
Luke Arms <luke@linacreative.com>
2022-01-19 10:09:05 +1100

    Add correction to LK_SMTP_SENDERS example in hosting.sh

--- lib/linode/hosting.sh
+++ lib/linode/hosting.sh
@@ -30,7 +30,7 @@ lk_die() { s=$? && echo "${0##*/}: $1" >&2 && (exit $s) && false || exit; }
 # <UDF name="LK_MEMCACHED_MEMORY_LIMIT" label="Memcached size" oneof="64,128,256,512,768,1024" default="256" />
 # <UDF name="LK_SMTP_RELAY" label="SMTP relay (system-wide)" example="[mail.clientname.com.au]:587" default="" />
 # <UDF name="LK_SMTP_CREDENTIALS" label="SMTP relay credentials" example="username:Passw0rd!" default="" />
-# <UDF name="LK_SMTP_SENDERS" label="Only relay email from these senders (comma-delimited)" example="linode.linacreative.com,linode@linacreative.com" default="" />
+# <UDF name="LK_SMTP_SENDERS" label="Only relay email from these senders (comma-delimited)" example="@linode.linacreative.com,linode@linacreative.com" default="" />
 # <UDF name="LK_EMAIL_DESTINATION" label="Only deliver email to this location (system-wide, STAGING ONLY)" example="/dev/null" default="" />
 # <UDF name="LK_UPGRADE_EMAIL" label="Email address for unattended upgrade notifications" example="unattended-upgrades@linode.linacreative.com" default="" />
 # <UDF name="LK_AUTO_REBOOT" label="Reboot automatically after unattended upgrades" oneof="Y,N" />
