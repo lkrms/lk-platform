d830a207af8ce272e5d5936f973781e17cab38d9
Luke Arms <luke@linacreative.com>
2022-05-10 11:14:33 +1000

    Fix bug in error-handling.php (`__return_false` not available anymore)

--- share/wp-cli/error-handling.php
+++ share/wp-cli/error-handling.php
@@ -2,7 +2,7 @@
 
 function lk_ini_set_error_reporting()
 {
-    ini_set("error_reporting", E_ALL & ~ E_WARNING & ~ E_NOTICE & ~ E_USER_WARNING & ~ E_USER_NOTICE & ~ E_STRICT & ~ E_DEPRECATED & ~ E_USER_DEPRECATED);
+    ini_set("error_reporting", E_ALL & ~E_WARNING & ~E_NOTICE & ~E_USER_WARNING & ~E_USER_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_USER_DEPRECATED);
     ini_set("display_errors", "stderr");
     ini_set("log_errors", false);
 }
@@ -15,13 +15,13 @@ $GLOBALS['wp_filter'] = [
     'enable_wp_debug_mode_checks' => [
         10 => [
             [
-                'function'      => '__return_false',
+                'function'      => function () { return false; },
                 'accepted_args' => 0,
             ],
         ],
     ],
     'plugin_loaded' => [
-        10 => [
+        10          => [
             [
                 'function'      => 'lk_ini_set_error_reporting',
                 'accepted_args' => 0,
@@ -29,4 +29,3 @@ $GLOBALS['wp_filter'] = [
         ],
     ],
 ];
-
