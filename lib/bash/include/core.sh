7d9849b973422bbcdb8753cda30583fbbdae9b52
Luke Arms <luke@linacreative.com>
2022-05-16 12:57:49 +1000

    Xfce: restart xfce4-panel in autorandr postswitch
    Also:
    - Add `lk_ps_running_seconds`

--- lib/bash/include/core.sh
+++ lib/bash/include/core.sh
@@ -2215,6 +2215,18 @@ function lk_ps_parent_command() {
     ps -o comm= -p "$PPID"
 }
 
+function lk_ps_running_seconds() {
+    # "elapsed time since the process was started, in the form [[DD-]hh:]mm:ss"
+    _lk_stream_args 6 xargs -r ps -o etime= -p "$@" | awk '
+      { d = t[1] = t[2] = t[3] = 0 }
+/-/   { split($1, a, /-/)
+        d  = a[1]
+        $1 = a[2] }
+      { n = split($1, a, /:/)
+        for(i in a) { t[n - i + 1] = a[i] }
+        print ((d * 24 + t[3]) * 60 + t[2]) * 60 + t[1] }'
+}
+
 # lk_ps_recurse_children [-p] PPID...
 #
 # Print the process ID of all processes descended from PPID. If -p is set,
