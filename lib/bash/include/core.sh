lkrms/lk-platform

commit 8bd532ed620a0de0f7104466cecdd96ba83ae793

Maintain exit status in chained EXIT traps
Fixes issue where backup hooks fail but job is a "success"

lib/bash/include/core.sh
@@ -1200,11 +1200,13 @@ function _lk_cleanup_on_exit() {
     local ARRAY=$1 COMMAND=$2
     shift 2
     [ -n "${!ARRAY+1}" ] ||
-        { COMMAND="{ [ -z \"\${${ARRAY}+1}\" ] ||
-    $COMMAND \"\${${ARRAY}[@]}\" || [ \"\$EUID\" -eq 0 ] ||
-    sudo $COMMAND \"\${${ARRAY}[@]}\" || true; } 2>/dev/null" &&
-            eval "$ARRAY=()" &&
-            lk_trap_add EXIT "$COMMAND" || return; }
+        eval "function ${ARRAY}_trap() {
+    local STATUS=\$?
+    { [ -z \"\${${ARRAY}+1}\" ] ||
+        $COMMAND \"\${${ARRAY}[@]}\" || [ \"\$EUID\" -eq 0 ] ||
+        sudo $COMMAND \"\${${ARRAY}[@]}\" || true; } 2>/dev/null
+    return \"\$STATUS\"
+} && $ARRAY=() && lk_trap_add EXIT ${ARRAY}_trap" || return
     eval "$ARRAY+=(\"\$@\")"
 }
 
@@ -3071,7 +3073,7 @@ function lk_lock() {
         eval "exec ${!2}>\"\$$1\"" || return
     flock ${_LK_NONBLOCK+-n} "${!2}" ||
         lk_warn "unable to acquire lock: ${!1}" || return
-    lk_trap_add EXIT lk_lock_drop "$@"
+    lk_trap_add EXIT lk_pass lk_lock_drop "$@"
 }
 
 # lk_lock_drop [LOCK_FILE_VAR LOCK_FD_VAR] [LOCK_NAME]
@@ -4511,6 +4513,7 @@ function _lk_exit_trap() {
                 $((1 - ${_LK_STACK_DEPTH:-0})) \
                 "$([ "${LK_NO_STACK_TRACE-}" != 1 ] || echo 1)" \
                 "${_LK_ERR_TRAP_CALLER-}")"
+    return "$STATUS"
 }
 
 function _lk_err_trap() {

src/lib/bash/core.sh.d/18-traps.sh
@@ -31,11 +31,13 @@ function _lk_cleanup_on_exit() {
     local ARRAY=$1 COMMAND=$2
     shift 2
     [ -n "${!ARRAY+1}" ] ||
-        { COMMAND="{ [ -z \"\${${ARRAY}+1}\" ] ||
-    $COMMAND \"\${${ARRAY}[@]}\" || [ \"\$EUID\" -eq 0 ] ||
-    sudo $COMMAND \"\${${ARRAY}[@]}\" || true; } 2>/dev/null" &&
-            eval "$ARRAY=()" &&
-            lk_trap_add EXIT "$COMMAND" || return; }
+        eval "function ${ARRAY}_trap() {
+    local STATUS=\$?
+    { [ -z \"\${${ARRAY}+1}\" ] ||
+        $COMMAND \"\${${ARRAY}[@]}\" || [ \"\$EUID\" -eq 0 ] ||
+        sudo $COMMAND \"\${${ARRAY}[@]}\" || true; } 2>/dev/null
+    return \"\$STATUS\"
+} && $ARRAY=() && lk_trap_add EXIT ${ARRAY}_trap" || return
     eval "$ARRAY+=(\"\$@\")"
 }
 

src/lib/bash/core.sh
@@ -527,7 +527,7 @@ function lk_lock() {
         eval "exec ${!2}>\"\$$1\"" || return
     flock ${_LK_NONBLOCK+-n} "${!2}" ||
         lk_warn "unable to acquire lock: ${!1}" || return
-    lk_trap_add EXIT lk_lock_drop "$@"
+    lk_trap_add EXIT lk_pass lk_lock_drop "$@"
 } #### Reviewed: 2021-05-23
 
 # lk_lock_drop [LOCK_FILE_VAR LOCK_FD_VAR] [LOCK_NAME]
@@ -1967,6 +1967,7 @@ function _lk_exit_trap() {
                 $((1 - ${_LK_STACK_DEPTH:-0})) \
                 "$([ "${LK_NO_STACK_TRACE-}" != 1 ] || echo 1)" \
                 "${_LK_ERR_TRAP_CALLER-}")"
+    return "$STATUS"
 } #### Reviewed: 2021-05-28
 
 function _lk_err_trap() {

bin/lk-backup-create-snapshot.sh
@@ -20,7 +20,7 @@ lk_require backup mail mysql
 
 function exit_trap() {
     local STATUS=$? MESSAGE TAR SUBJECT
-    [ "$BASH_SUBSHELL" -eq 0 ] || return
+    [ "$BASH_SUBSHELL" -eq 0 ] || return "$STATUS"
     exec 8>&- &&
         rm -Rf "${FIFO_FILE%/*}" || true
     lk_log_close -r
@@ -81,6 +81,7 @@ $(lk_strip_non_printing -d '\v' <"$SNAPSHOT_LOG_FILE")" &&
             lk_mail_set_text "$MESSAGE" &&
             lk_mail_send "$SUBJECT" "$LK_BACKUP_MAIL" "$LK_BACKUP_MAIL_FROM" || true
     }
+    return "$STATUS"
 }
 
 function find_custom() {

bin/lk-provision-macos.sh
@@ -23,9 +23,10 @@ lk_die() { s=$? && echo "$0: ${1-error $s}" >&2 && (exit $s) && false || exit; }
 umask 002
 
 function exit_trap() {
-    local EXT _LOG_FILE LOG_FILE _LK_LOG_BASENAME=lk-provision-macos.sh \
+    local STATUS=$? EXT \
+        _LOG_FILE LOG_FILE _LK_LOG_BASENAME=lk-provision-macos.sh \
         _LOG=$_LK_LOG_FILE_LOG _OUT=$_LK_LOG_FILE_OUT
-    lk_log_close || return
+    lk_log_close || return "$STATUS"
     for EXT in log out; do
         _LOG_FILE=_$(lk_upper "$EXT") &&
             _LOG_FILE=${!_LOG_FILE} &&
@@ -36,6 +37,7 @@ function exit_trap() {
             rm "$_LOG_FILE" ||
             lk_tty_warning "Error moving" "$_LOG_FILE"
     done
+    return "$STATUS"
 }
 
 {

contrib/legacy/backup/create-rsync-snapshot.sh
@@ -253,6 +253,7 @@ $(sed \
             lk_mail_send "$SUBJECT" "$LK_BACKUP_MAIL" "$LK_BACKUP_MAIL_FROM" || true
         [ -z "${TAR-}" ] || rm -f "$TAR" || true
     }
+    return "$STATUS"
 }
 
 function find_custom() {

lib/arch/bootstrap.sh
@@ -240,9 +240,8 @@ fi
 lk_tty_print
 
 function exit_trap() {
-    if [ "$BASH_SUBSHELL" -gt 0 ]; then
-        return
-    fi
+    local STATUS=$?
+    [ "$BASH_SUBSHELL" -eq 0 ] || return "$STATUS"
     [ ! -d /mnt/boot ] || {
         set +x
         unset BASH_XTRACEFD
@@ -259,6 +258,7 @@ function exit_trap() {
         lk_tty_log \
             "The random password generated for user '$BOOTSTRAP_USERNAME' is" \
             "$BOOTSTRAP_PASSWORD"
+    return "$STATUS"
 }
 
 function in_target() {

commit 44ced28767cc518a517ee6fe32f1e29cd8a318cc

Fix issue where some exit traps fail to run (regression in 8bd532e)
- `lk_trap_add`: always run all traps instead of chaining them with `&&`

lib/bash/include/core.sh
@@ -1241,12 +1241,8 @@ function lk_mktemp_dir_with() {
 
 # lk_trap_add SIGNAL COMMAND [ARG...]
 function lk_trap_add() {
-    [ $# -ge 2 ] ||
-        lk_usage "Usage: $FUNCNAME SIGNAL COMMAND [ARG...]" || return
-    local IFS
-    unset IFS
-    [ $# -eq 2 ] ||
-        set -- "$1" "$2$(printf ' %q' "${@:3}")"
+    (($# > 1)) || lk_err "invalid arguments" || return
+    set -- "$1" "$2${3+ $(shift 2 && lk_quote_args "$@")}"
     _LK_TRAPS=(${_LK_TRAPS+"${_LK_TRAPS[@]}"})
     local i TRAPS=()
     for ((i = 0; i < ${#_LK_TRAPS[@]}; i += 3)); do
@@ -1256,14 +1252,13 @@ function lk_trap_add() {
         [[ ${_LK_TRAPS[i + 2]} != "${2-}" ]] ||
             set -- "$1"
     done
-    [ $# -eq 1 ] || {
+    (($# == 1)) || {
         TRAPS[${#TRAPS[@]}]=$2
         _LK_TRAPS+=("$BASH_SUBSHELL" "$1" "$2")
     }
-    trap -- "$(
-        printf '{ %s; }' "$TRAPS"
-        [ "${#TRAPS[@]}" -lt 2 ] || printf ' && { %s; }' "${TRAPS[@]:1}"
-    )" "$1"
+    trap -- "declare _LK_TRAP_STATUS=0;$(
+        printf '(%s)||_LK_TRAP_STATUS=$?;' "${TRAPS[@]}"
+    )(exit \$_LK_TRAP_STATUS)" "$1"
 }
 
 function _lk_cleanup_on_exit() {

src/lib/bash/core.sh.d/18-traps.sh
@@ -2,12 +2,8 @@
 
 # lk_trap_add SIGNAL COMMAND [ARG...]
 function lk_trap_add() {
-    [ $# -ge 2 ] ||
-        lk_usage "Usage: $FUNCNAME SIGNAL COMMAND [ARG...]" || return
-    local IFS
-    unset IFS
-    [ $# -eq 2 ] ||
-        set -- "$1" "$2$(printf ' %q' "${@:3}")"
+    (($# > 1)) || lk_err "invalid arguments" || return
+    set -- "$1" "$2${3+ $(shift 2 && lk_quote_args "$@")}"
     _LK_TRAPS=(${_LK_TRAPS+"${_LK_TRAPS[@]}"})
     local i TRAPS=()
     for ((i = 0; i < ${#_LK_TRAPS[@]}; i += 3)); do
@@ -17,14 +13,13 @@ function lk_trap_add() {
         [[ ${_LK_TRAPS[i + 2]} != "${2-}" ]] ||
             set -- "$1"
     done
-    [ $# -eq 1 ] || {
+    (($# == 1)) || {
         TRAPS[${#TRAPS[@]}]=$2
         _LK_TRAPS+=("$BASH_SUBSHELL" "$1" "$2")
     }
-    trap -- "$(
-        printf '{ %s; }' "$TRAPS"
-        [ "${#TRAPS[@]}" -lt 2 ] || printf ' && { %s; }' "${TRAPS[@]:1}"
-    )" "$1"
+    trap -- "declare _LK_TRAP_STATUS=0;$(
+        printf '(%s)||_LK_TRAP_STATUS=$?;' "${TRAPS[@]}"
+    )(exit \$_LK_TRAP_STATUS)" "$1"
 }
 
 function _lk_cleanup_on_exit() {