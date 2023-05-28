a31054fbbad4824726f375ae0087dc68f083b577
Luke Arms <luke@linacreative.com>
2023-05-28 18:26:14 +1000

    `plist_sort.py`: sort strings case-insensitively

--- lib/python/plist_sort.py
+++ lib/python/plist_sort.py
@@ -12,6 +12,7 @@ when they are set to '-'. Use '.' between parent and child properties in
 `to_sort` and `sort_by` if needed.
 
 Example:
+  # Sort Dash docsets alphabetically
   plist_sort.py com.kapeli.dashdoc.plist - docsets docsetName
 """
 
@@ -51,25 +52,30 @@ def dict_set(dct, path, val):
         dct.update(val)
 
 
+def maybe_lower(val):
+    if isinstance(val, str):
+        return val.lower()
+    else:
+        return val
+
+
 def plist_sort(in_file, out_file, sort, by):
     in_file = sys.stdin.buffer if in_file == "-" else open(in_file, "rb")
     plist = plistlib.loads(in_file.read())
     in_file.close()
     data = dict_get(plist, sort)
     if isinstance(data, dict):
-        data = dict(sorted(
-            data.items(),
-            key=lambda item: item[0]
-        ))
+        data = dict(sorted(data.items(), key=lambda item: maybe_lower(item[0])))
     else:
         if has_builtin_method(data, "copy"):
             data = data.copy()
-        data = sorted(
-            data,
-            key=lambda item: dict_get(item, by)
-        )
+        data = sorted(data, key=lambda item: maybe_lower(dict_get(item, by)))
     dict_set(plist, sort, data)
-    format = plistlib.PlistFormat.FMT_XML if out_file == "-" else plistlib.PlistFormat.FMT_BINARY
+    format = (
+        plistlib.PlistFormat.FMT_XML
+        if out_file == "-"
+        else plistlib.PlistFormat.FMT_BINARY
+    )
     out_file = sys.stdout.buffer if out_file == "-" else open(out_file, "wb")
     plistlib.dump(plist, out_file, fmt=format, sort_keys=False)
     out_file.close()
