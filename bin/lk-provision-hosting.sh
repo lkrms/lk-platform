f865f5b3a514293a23f2f1da29b021074a6d10fa
Luke Arms <luke@linacreative.com>
2024-08-20 17:25:47 +1000

    Hosting: fix issue where service changes are not loaded immediately

--- bin/lk-provision-hosting.sh
+++ bin/lk-provision-hosting.sh
@@ -1137,12 +1137,15 @@ END        { if (m) { print u[m] } else { exit 1 } }')} ||
                 "$LK_BASE/share/mariadb.conf.d/default-hosting.template.cnf"
         )
         lk_file_replace "$FILE" "$_FILE"
+        CONFIG_NO_CHANGE=$LK_FILE_REPLACE_NO_CHANGE
+        unset LK_FILE_REPLACE_NO_CHANGE
         FILE=/etc/systemd/system/mariadb.service.d/90-${LK_PATH_PREFIX}override.conf
         lk_install -d -m 00755 "${FILE%/*}"
         lk_install -m 00644 "$FILE"
         lk_file_replace \
             -f "$LK_BASE/share/systemd/mariadb.service" \
             "$FILE"
+        SERVICE_NO_CHANGE=$LK_FILE_REPLACE_NO_CHANGE
         if lk_is_bootstrap; then
             # MariaDB packages provide a `mysql.service` alias for
             # `mariadb.service`, so use `mysql.service` for maximum portability
@@ -1158,8 +1161,11 @@ WITH GRANT OPTION
 EOF
             fi
         else
-            ! lk_false LK_FILE_REPLACE_NO_CHANGE ||
+            if lk_false CONFIG_NO_CHANGE || lk_false SERVICE_NO_CHANGE; then
+                ! lk_false SERVICE_NO_CHANGE ||
+                    lk_tty_run_detail systemctl daemon-reload
                 lk_tty_run_detail systemctl restart mysql.service
+            fi
         fi
     fi
 
