af2fecde22b575b43e316bb1471fff18c5f01496
Luke Arms <luke@linacreative.com>
2025-11-29 14:19:02 +1100

    Improve `analyse-access-log.sh`
    - Sort log files by modified date
    - Preserve original order of entries
    - Add percentage columns after `request_count` and `bytes_sent`
    - Also generate summary of entries by network
    - Add headings to columns

--- contrib/hosting/analyse-access-log.sh
+++ contrib/hosting/analyse-access-log.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=2329
 function __usage() {
     cat <<EOF
 Analyse Apache logs on a hosting server, reporting request count and bytes sent
@@ -14,6 +15,35 @@ Each <DB_PATH> must resolve to a local file with the \`mmdb\` extension.
 If given, each <SOURCE_LOG_PATH> must resolve to a readable \`access.log\` file
 on the hosting server, otherwise every available \`access.log\` file in
 \`~/log\` on the remote system is included in the analysis.
+
+Output (tab-delimited):
+   1. IP address
+   2. Request count
+   3. Request count (%)
+   4. Bytes sent
+   5. Bytes sent (%)
+   6. First request time (e.g. \`[03/Oct/2025:06:27:02 +1000]\`)
+   7. Last request time
+   8. User agent (extracted from last request per IP)
+   9. Country (or \`-\`)
+  10. City (or \`-\`)
+  11. Network (or \`-\`)
+  12. ASN (or \`-\`)
+  13. Organisation (or \`-\`)
+
+Output by network (tab-delimited, written to separate file):
+   1. Network (or \`-\`)
+   2. Request count
+   3. Request count (%)
+   4. Bytes sent
+   5. Bytes sent (%)
+   6. Country (or \`-\`)
+   7. City (or \`-\`)
+   8. ASN (or \`-\`)
+   9. Organisation (or \`-\`)
+  10. First request time (e.g. \`[03/Oct/2025:06:27:02 +1000]\`)
+  11. Last request time (from last output entry per network)
+  12. User agent (from last output entry per network)
 EOF
 }
 
@@ -35,19 +65,31 @@ lk_require provision
     done
     [[ ${DB+1} ]] || lk_usage -e "invalid or missing DB_PATH"
 
+    # shellcheck disable=2329
     function summarise_access_log() {
         shopt -s nullglob extglob
         (($#)) || set -- ~/log/access.log{,.+([0-9]),.+([0-9]).gz}
-        while (($#)); do
-            [[ -e $1 ]] || continue
-            if [[ $1 == *.gz ]]; then
-                zcat "$1"
+        local FILE
+        while IFS= read -r FILE; do
+            echo "Reading $FILE" >&2
+            if [[ $FILE == *.gz ]]; then
+                zcat "$FILE"
             else
-                cat "$1"
+                cat "$FILE"
             fi
-            shift
-        done | awk '
+        done < <(
+            # Sort files by modified date
+            if [[ $OSTYPE != darwin* ]]; then
+                stat -c '%Y :%n' -- "$@"
+            else
+                stat -t '%s' -f '%Sm :%N' -- "$@"
+            fi | sort -n | cut -d: -f2-
+        ) | awk '
 {
+  if (! request_count[$1]) {
+    total_ips++
+    ips[total_ips] = $1
+  }
   request_count[$1]++
   bytes_sent[$1] += $10
   last_request_time[$1] = $4 " " $5
@@ -55,25 +97,30 @@ lk_require provision
     first_request_time[$1] = last_request_time[$1]
   }
   last_line[$1] = $0
+  total_request_count++
+  total_bytes_sent += $10
 }
 
 END {
   OFS = "\t"
-  for (ip in request_count) {
+  print "IP address", "Request count", "Request count (%)", "Bytes sent", "Bytes sent (%)", "First request time", "Last request time", "User agent (last seen)", "Country", "City", "Network", "ASN", "Organisation"
+  for (i in ips) {
+    ip = ips[i]
     split(last_line[ip], parts, /"/)
     last_user_agent = parts[6]
-    print request_count[ip], bytes_sent[ip], ip, first_request_time[ip], last_request_time[ip], last_user_agent
+    print ip, request_count[ip], sprintf("%.6f", request_count[ip] * 100 / total_request_count), bytes_sent[ip], sprintf("%.6f", bytes_sent[ip] * 100 / total_bytes_sent), first_request_time[ip], last_request_time[ip], last_user_agent
   }
-}' | gzip
+}
+' | gzip
     }
 
     lk_mktemp_with SUMMARY
     lk_tty_print "Retrieving access.log summary"
-    lk_ssh_run_on_host "$SOURCE" summarise_access_log "$@" | gunzip | sort -n -k1,1 >"$SUMMARY"
+    lk_ssh_run_on_host "$SOURCE" summarise_access_log "$@" | gunzip >"$SUMMARY"
 
     lk_mktemp_with DATA
     lk_tty_print "Extracting IP address data"
-    awk '{ print $3 }' "$SUMMARY" |
+    awk 'NR > 1 { print $1 }' "$SUMMARY" |
         xargs mmdbinspect "${DB[@]/#/-db=}" -jsonl |
         jq -rs '
 reduce(.[] | {
@@ -107,7 +154,7 @@ reduce(.[] | {
     ] |
     @tsv' >"$DATA"
 
-    BACKUP=~/.lk-platform/cache/log/${0##*/}-$(lk_date_ymdhms).gz
+    BACKUP=~/.lk-platform/cache/log/${0##*/}-$(lk_date_ymdhms).csv.gz
     install -d -m 00700 "${BACKUP%/*}"
     lk_tty_print "Merging data"
     awk '
@@ -118,11 +165,54 @@ FILENAME == ARGV[1] {
   next
 }
 
+FNR == 1 {
+  print
+  next
+}
+
 {
-  print $0 data[$3]
+  print $0 data[$1]
 }
 ' "$DATA" "$SUMMARY" | tee >(gzip >"$BACKUP")
-    lk_tty_detail "Output also written to" "$BACKUP"
+    lk_tty_detail "Output:" "$BACKUP"
+
+    BACKUP2=${BACKUP%.csv.gz}-by-network.csv.gz
+    cat "$BACKUP" | gunzip | awk '
+BEGIN {
+  FS = "\t"
+}
+
+NR > 1 {
+  if (! request_count[$11]) {
+    total_networks++
+    networks[total_networks] = $11
+  }
+  request_count[$11]++
+  bytes_sent[$11] += $4
+  if (! first_request_time[$11]) {
+    first_request_time[$11] = $6
+    country[$11] = $9
+    city[$11] = $10
+    asn[$11] = $12
+    organisation[$11] = $13
+  }
+  # TODO: convert to timestamp and keep most recent
+  last_request_time[$11] = $7
+  last_user_agent[$11] = $8
+  total_request_count++
+  total_bytes_sent += $4
+}
+
+END {
+  OFS = "\t"
+  print "Network", "Request count", "Request count (%)", "Bytes sent", "Bytes sent (%)", "Country", "City", "ASN", "Organisation", "First request time", "Last request time", "User agent (last seen)"
+  for (i in networks) {
+    n = networks[i]
+    print n, request_count[n], sprintf("%.6f", request_count[n] * 100 / total_request_count), bytes_sent[n], sprintf("%.6f", bytes_sent[n] * 100 / total_bytes_sent), country[n], city[n], asn[n], organisation[n], first_request_time[n], last_request_time[n], last_user_agent[n]
+  }
+}
+' | gzip >"$BACKUP2"
+    lk_tty_detail "Output by network:" "$BACKUP2"
 
     exit
 }
